7b4fb1e329f5eb12a0da82135e7462922dfe8c19
vikashkumar4-max <vikash.kumar4@wheelseye.com>
2026-08-07 16:13:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ supported_formats = [
     "%d.%m.%Y", "%Y.%m.%d"
 ]
 
-input_date = please paste date here 
+input_date = " Please paste date here "
 
 # Parse input string to standard date object
 if user_date_str.strip():
